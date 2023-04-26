454967d578c12cb6d37ce1bebea414854c9eaf7a
M0thMh <m64m76@gmail.com>
2023-04-26 17:32:15 +0300

    fixing dmenu always centered

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar 		    = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered 		    = 1;                      /* -c option; centers dmenu on screen */
+static int centered 		    = 0;                      /* -c option; centers dmenu on screen */
 static int min_width 		    = 500;                    /* minimum width when centered */
 static unsigned int border_width    = 1;                      /* Size of the window border */
 
--- dwm/config.h
+++ dwm/config.h
@@ -75,7 +75,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run",  "-m", dmenumon, NULL };
+static const char *dmenucmd[] = { "dmenu_run",  "-m", dmenumon, "-c", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
